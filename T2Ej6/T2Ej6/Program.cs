9f8f466ed2964a1144570e82d580818eed03e495
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-19 21:22:46 -0600

    Add files via upload

---
+++ T2Ej6/T2Ej6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T2Ej6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int a, num;
+			Console.Write("Ingrese un n\u00FAmero:");
+			num = int.Parse(Console.ReadLine());
+			for (a = 2; a < num && num % a != 0; a++) ;
+			if (a == num)
+				Console.WriteLine("\nW1 n\u00FAmero " + num + " s\u00ED es primo.");
+			else
+				Console.WriteLine("\nW1 n\u00FAmero " + num + " s\u00ED no es primo.");
+			Console.WriteLine("\nPresione una tecla para terminar . . . ");
+			Console.ReadKey();
+		}
+    }
+}
