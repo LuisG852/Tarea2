a2640fb14552284b4d58d88c1806d5a87e758e7e
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-19 21:18:50 -0600

    Add files via upload

---
+++ T2Ej3/T2Ej3/T2Ej3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T2Ej3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, total;
+            Console.Write("Introduzca las horas laboradas de la semana: ");
+            horas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            if (horas < 39)
+            {
+                total = horas * 16;
+                Console.Write("Usted trabajo " + horas + "horas. Y su salario semanal es de: " + total);
+                Console.WriteLine("");
+            }
+            else if (horas > 40)
+            {
+                total = ((horas - 40) * 20) + (40 * 16);
+                Console.WriteLine("Su salario es de: " + total);
+            }
+        }
+    }
+}
