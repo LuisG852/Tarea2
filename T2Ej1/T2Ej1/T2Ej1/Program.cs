af13bb523fbef07ae56437c66626a468a2d9263d
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-19 20:59:30 -0600

    Add files via upload

---
+++ T2Ej1/T2Ej1/T2Ej1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T2Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Letra;
+            Console.Write("Introduzca una Letra: ");
+            Letra = Convert.ToChar(Console.ReadLine());
+            if (char.IsUpper(Letra))
+            {
+                Console.WriteLine("La letra ingresada es Mayuscula");
+
+
+            }
+            else
+            {
+                Console.WriteLine("La letra ingresada es Minuscula");
+            } 
+        }
+    }
+}
