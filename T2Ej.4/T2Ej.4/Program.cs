b107871ae2fe5ebbd0c977669aa5305b1fcd3a7d
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-19 21:19:30 -0600

    Add files via upload

---
+++ T2Ej.4/T2Ej.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T2Ej._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio, dia, dias, mes;
+            Console.Write("Ingrese el valor de anno: ");
+            anio = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de dia: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el valor de mes: ");
+            mes = int.Parse(Console.ReadLine());
+            dias = 0;
+            if (mes == 2 && (((anio % 4 == 0) && (anio % 100 != 0)) || anio % 400 == 0))
+                dias = 29;
+            if (mes == 2 && (((anio % 4 != 0) || (anio % 100 == 0)) && anio % 400 != 0))
+                dias = 28;
+            if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+                dias = 31;
+            if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+                dias = 30;
+            Console.WriteLine("Valor de dias del mes: " + dias);
+            Console.WriteLine();
+            Console.Write("Presione una tecla para terminar . . . ");
+            Console.ReadKey();
+        }
+    }
+}
