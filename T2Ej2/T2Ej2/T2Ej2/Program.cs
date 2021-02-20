deb0e1316d9882198da96ba9dd832cd17115ddc6
LuisG852 <79346137+LuisG852@users.noreply.github.com>
2021-02-19 20:57:20 -0600

    Add files via upload

---
+++ T2Ej2/T2Ej2/T2Ej2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T2Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double desc, total;
+            Console.Write("Ingrese el Total de la compra para un descuento: ");
+            total = Convert.ToInt32(Console.ReadLine());
+            desc = total * 0.20;
+            if (total >= 300)
+            {
+                Console.WriteLine("Su descuento es de: " + desc);
+
+            }
+            else
+            {
+                Console.WriteLine("Usted no tiene ningun descuento");
+            }
+        }
+    }
+}
